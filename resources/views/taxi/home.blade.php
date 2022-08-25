88260dd12bf8833016aa3a76069ae83ae749ae51
Andrii <sferaved@gmail.com>
2022-08-25 15:54:56 +0300

    Task 16

--- resources/views/taxi/home.blade.php
+++ resources/views/taxi/home.blade.php
@@ -4,12 +4,13 @@
     <div class="px-1 py-1 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
     <div class="container">
         <main>
-            <div class="py-1 text-center">
-                <h2>Заповнити поля для розрахунку вартості поїздки.</h2>
+            <div class="text-center">
+                <p class="lead">Заповнити поля для розрахунку вартості поїздки.</p>
             </div>
             <form action="{{route('search-cost')}}">
                 @csrf
                 <div class="row g-5">
+
                     <div class="col-md-7 col-lg-8">
 
                             <div class="row g-3">
@@ -22,34 +23,34 @@
                                     <input type="hidden" id="user_full_name" name="user_full_name" placeholder="Андрій"  class="form-control" value="Новий замовник">
 
                                 </div>
-                                <div class="col-sm-8">
+                                <div class="col-sm-11">
                                     <label for="search" class="form-label">Звідки</label>
                                     <input type="text" class="form-control" id="search" name="search" placeholder="Пошук вулиці" value="" required>
                                 </div>
 
-                                <div class="col-sm-4">
+                                <div class="col-sm-1">
                                     <label for="from_number" class="form-label">Будинок</label>
                                     <input type="text" id="from_number" name="from_number" placeholder="1" class="form-control" value="" required/>
 
                                 </div>
-                                <div class="col-sm-8">
+                                <div class="col-sm-11">
                                     <label for="search1" class="form-label">Куди</label>
                                     <input type="text" class="form-control" id="search1" name="search1" placeholder="Пошук вулиці" value="" required>
                                 </div>
 
-                                <div class="col-sm-4">
+                                <div class="col-sm-1">
                                     <label for="to_number" class="form-label">Будинок</label>
                                     <input type="text" id="to_number" name="to_number" placeholder="1" class="form-control" value="" required/>
 
                                 </div>
-                                <div class="col-sm-8">
+                                <div class="col-sm-11">
 <!--                                    <label for="comment" class="form-label">Коментар</label>-->
                                     <input type="hidden" class="form-control" id="comment" name="comment" placeholder="Додати побажання" />
 <!--                                    <textarea type="hidden" class="form-control" id="comment" name="comment" placeholder="Додати побажання"></textarea>-->
 
                                 </div>
 
-                                <div class="col-sm-4">
+                                <div class="col-sm-1">
 <!--                                    <label for="add_cost" class="form-label">Додати до вартості (грн)</label>-->
                                     <input type="hidden" id="add_cost" name="add_cost" value="0" class="form-control" />
                                 </div>
@@ -184,54 +185,58 @@
                             </div>
 -->
 <!--                            <hr class="my-4">-->
+
+
+
                     </div>
                     <div class="col-md-5 col-lg-4 order-md-last">
-                        <h4 class="d-flex justify-content-between align-items-center mb-3">
-                            <span class="text-primary">Бажаний тип авто</span>
-                        </h4>
-                        <ul class="list-group mb-3">
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="wagon" name="wagon">
-                                    <label class="form-check-label" for="wagon">Универсал</label>
-                                </div>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="minibus" name="minibus">
-                                    <label class="form-check-label" for="minibus">Мікроавтобус</label>
-                                </div>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="premium" name="premium">
-                                    <label class="form-check-label" for="premium">Машина преміум-класса</label>
-                                </div>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div class="col-md-12">
-                                    <label for="$flexible_tariff_name" class="form-label">Тариф</label>
-                                    <select class="form-select" id="flexible_tariff_name" name="flexible_tariff_name" required>
-                                        @for ($i = 0; $i < count($json_arr); $i++)
-                                            <option>{{$json_arr[$i]['name']}}</option>
-                                        @endfor
-                                    </select>
-                                </div>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div class="col-md-12">
-                                    <label for="$flexible_tariff_name" class="form-label">Тип оплати замовлення</label>
-                                    <select class="form-select" id="flexible_tariff_name" name="payment_type" required>
-                                        <option>готівка</option>
-                                        <option>безготівка</option>
-                                    </select>
-                                </div>
-                            </li>
-                        </ul>
+
+                            <a href="javascript:void(0)" class="btn btn-outline-success col-12 order-md-last" onclick="showHide('block_id')">Бажаний тип авто</a><br/><br/>
+
+                        <div id="block_id" style="display: none;">
+                            <ul class="list-group mb-3">
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="wagon" name="wagon">
+                                        <label class="form-check-label" for="wagon">Универсал</label>
+                                    </div>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="minibus" name="minibus">
+                                        <label class="form-check-label" for="minibus">Мікроавтобус</label>
+                                    </div>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="premium" name="premium">
+                                        <label class="form-check-label" for="premium">Машина преміум-класса</label>
+                                    </div>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div class="col-md-12">
+                                        <label for="$flexible_tariff_name" class="form-label">Тариф</label>
+                                        <select class="form-select" id="flexible_tariff_name" name="flexible_tariff_name" required>
+                                            @for ($i = 0; $i < count($json_arr); $i++)
+                                                <option>{{$json_arr[$i]['name']}}</option>
+                                            @endfor
+                                        </select>
+                                    </div>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div class="col-md-12">
+                                        <label for="$flexible_tariff_name" class="form-label">Тип оплати замовлення</label>
+                                        <select class="form-select" id="flexible_tariff_name" name="payment_type" required>
+                                            <option>готівка</option>
+                                            <option>безготівка</option>
+                                        </select>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
 
-                </div>
-                <button class="w-100 btn btn-primary btn-lg" type="submit">Розрахувати вартість поїздки</button>
+                </div><button class="w-100 btn btn-primary btn-lg" type="submit">Розрахувати вартість поїздки</button>
             </form>
         </main>
         <footer class="my-5 pt-5 text-muted text-center text-small">
@@ -245,6 +250,27 @@
 
     </div>
     </div>
+
+    <script type="text/javascript">
+        /**
+         * Функция Скрывает/Показывает блок
+         * @author ox2.ru дизайн студия
+         **/
+        function showHide(element_id) {
+            //Если элемент с id-шником element_id существует
+            if (document.getElementById(element_id)) {
+                //Записываем ссылку на элемент в переменную obj
+                var obj = document.getElementById(element_id);
+                //Если css-свойство display не block, то:
+                if (obj.style.display != "block") {
+                    obj.style.display = "block"; //Показываем элемент
+                }
+                else obj.style.display = "none"; //Скрываем элемент
+            }
+            //Если элемент с id-шником element_id не найден, то выводим сообщение
+            else alert("Элемент с id: " + element_id + " не найден!");
+        }
+    </script>
 
 
 @endsection
