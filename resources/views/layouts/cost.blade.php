e3b0169b6c74c45ddf6eedb8dc7b233e660d3212
Andrii <sferaved@gmail.com>
2022-09-11 13:24:16 +0300

    fix google script

--- resources/views/layouts/cost.blade.php
+++ resources/views/layouts/cost.blade.php
@@ -12,17 +12,7 @@
         <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
         <!-- Global site tag (gtag.js) - Google Ads: 999615800 -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-999615800"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
 
-            gtag('config', 'AW-999615800');
-        </script>
-    <!-- Event snippet for Website traffic conversion page -->
-    <script defer>
-        gtag('event', 'conversion', {'send_to': 'AW-999615800/bAbGCJLlkNsDELja09wD'});
-    </script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
