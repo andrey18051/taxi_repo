7af5adc9c2c81b253e44a7606362fe6a6f39df2a
Andrii <sferaved@gmail.com>
2022-10-02 11:14:18 +0300

    SVG register and $

--- resources/views/layouts/taxiWelcome2.blade.php
+++ resources/views/layouts/taxiWelcome2.blade.php
@@ -41,6 +41,9 @@
                     <!-- Links -->
                     <li class="nav-item dropdown">
                         <a id="navbar" class="nav-link" href="{{route('homeStreet', [$phone, $user_name])}}" role="button" target="_blank" data-bs-toggle="#" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                            </svg>
                             {{ __('Замовлення') }}
                         </a>
                     </li>
@@ -53,11 +56,14 @@
                             {{ __('Вхід') }}
                         </a>
                     </li>
-                    <li class="nav-item dropdown">
+<!--                    <li class="nav-item dropdown">
                         <a id="navbar" class="nav-link" href="{{ route('registration-sms', '000') }}" role="button" target="_blank" data-bs-toggle="#" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-r-circle" viewBox="0 0 16 16">
+                                <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM5.5 4.002h3.11c1.71 0 2.741.973 2.741 2.46 0 1.138-.667 1.94-1.495 2.24L11.5 12H9.98L8.52 8.924H6.836V12H5.5V4.002Zm1.335 1.09v2.777h1.549c.995 0 1.573-.463 1.573-1.36 0-.913-.596-1.417-1.537-1.417H6.835Z"/>
+                            </svg>
                             {{ __('Реєстрація') }}
                         </a>
-                    </li>
+                    </li>-->
 
                 </ul>
             </div>
