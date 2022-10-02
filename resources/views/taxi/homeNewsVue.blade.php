9b186d69187d7f9213139d94c71e8eec059bab23
Andrii <sferaved@gmail.com>
2022-10-02 16:17:08 +0300

    reklama

--- resources/views/taxi/homeNewsVue.blade.php
+++ resources/views/taxi/homeNewsVue.blade.php
@@ -2,21 +2,45 @@
 
 @section('content')
     <body>
+
     <div class="container">
-        <a class="but_vart" href="http://taxi2012" target="_blank" title="Версія сайту для мобільних">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
-                <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
-                <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-            </svg>
-        </a>
-    </div>
-    <div class="container">
-        <a class="but_vart_vart" href="{{route('homeStreet', [$phone = '000', $user_name = 'Новий замовник'])}}" target="_blank" title="Розрахунок вартості">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
-                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
-            </svg>
-        </a>
-    </div>
+        <div class="row">
+            <div class="col-2">
+                <p class="gradient">Корисні посилання</p>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Тариф узнайте на сайте такси</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Дешевое такси онлайн расчет</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Онлайн свободные авто такси</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Онлайн такси заказ Киев и обл</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Онлайн такси заказ Киев и обл</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Заказ такси Киев и область</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Онлайн дешевле можно только пешком. Закажите украинское такси и дайте свою оценку</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Эконом служба Киев - быстро, доступно, недорого</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Мы не ложим болт на заказчиков, при дешевых ценах</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Качественный сервис по низким ценам быстрее чем через оператор</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Тариф узнайте на сайте такси</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Тариф узнайте на сайте такси</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Тариф узнайте на сайте такси</a>
+                <a href="https://m.easy-order-taxi.site/home-Street/000/Новий%20замовник">Тариф узнайте на сайте такси</a>
+            </div>
+         <div class="col-8">
+            <div class="container">
+                <a class="but_vart" href="http://taxi2012" target="_blank" title="Версія сайту для мобільних">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+                        <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+                        <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                    </svg>
+                </a>
+            </div>
+            <div class="container">
+                <a class="but_vart_vart" href="{{route('homeStreet', [$phone = '000', $user_name = 'Новий замовник'])}}" target="_blank" title="Розрахунок вартості">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                        <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+                    </svg>
+                </a>
+            </div>
+
+
+
     <?php
     $connection = new PDO('mysql:host=localhost;dbname=taxi2012;charset=utf8', 'root', '');
     //   $connection = new PDO('mysql:host=127.0.0.1:3310;dbname=admin_taxi;charset=utf8', 'admin_taxi', 'MR5nTc9R8h');
@@ -70,8 +94,8 @@
 
 
     <div style="text-align:center">
-        <!--   <a href="https://easy-order-taxi.site/mainpage" class="gradient-button" target="_blank">Замовити таксі</a>-->
-     <a href="http://taxi2012" class="gradient-button" target="_blank">Замовити таксі</a>
+          <a href="https://easy-order-taxi.site/mainpage" class="gradient-button" target="_blank">Замовити таксі</a>
+    <!--<a href="http://taxi2012" class="gradient-button" target="_blank">Замовити таксі</a>-->
     </div>
 
 
@@ -81,7 +105,10 @@
         <cite>Цитата дня</cite>
     </blockquote>
 
+         </div>
+        </div>
 
+    </div>
     </body>
 
 
