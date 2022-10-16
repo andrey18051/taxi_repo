37d912f9fcf974f07615dd6b3b8e5a0db3c057e1
Andrii <sferaved@gmail.com>
2022-10-16 15:20:57 +0300

    Fix news page

--- resources/views/taxi/homeNewsVue.blade.php
+++ resources/views/taxi/homeNewsVue.blade.php
@@ -54,7 +54,7 @@
                         Ознайомтесь із популярними послугами нашої служби. Доступна вартість кожної поїздки найбільшого парку комфортних автомобілів.
                     </p>
                     <img src="{{ asset('img/kiyv2.jpg') }}" style="width: 100%; height: auto"></a>
-                <a href="{{route('home')}}" class="gradient-button" target="_blank">Замовити таксі</a>
+
             </div>
 
             <div class="col-9">
@@ -69,6 +69,7 @@
                                           <small style="text-align: left">
                                               Можливість оформити замовлення таксі на бажаний час та день.
                                               Зручно при поїздках до аеропортів та вокзалів Києва. Швидко, надійно та недорого.
+                                              Розрахуйте точну вартість на сайті.
                                           </small>
                                         </a>
                                     </li>
@@ -76,37 +77,8 @@
                                 <div class="col-4">
                                     <li><a href="#"><b>Послуга "тверезий водій"<br></b>
                                             <small style="text-align: left">
-                                                Професійний водій нашої служби акуратно та швидко пережене за вказаною адресою або на найближче паркування Ваш автомобіль.
-                                            </small>
-                                        </a>
-                                    </li>
-                                </div>
-                                <div class="col-4">
-                                   <li><a href="#"><b>Зустріч з табличкою<br></b>
-                                           <small style="text-align: left">
-                                               Водій зустріне вас з табличкою біля виходу з паспортного контролю в аеропорту або біля виходу з вагона на вокзалі.
-                                           </small>
-                                       </a>
-                                   </li>
-                               </div>
-
-                            </div>
-                        </div>
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-4">
-                                    <li><a href="#"><b>Таксі в аеропорт Бориспіль та Жуляни<br></b>
-                                            <small style="text-align: left">
-                                                Оцініть  найвигідніші ціни та якісний сервіс на поїздку в аеропорти міста Києва. Розрахуйте вартість поїздки в таксі до аеропорту онлайн.
-                                            </small>
-                                        </a>
-                                    </li>
-                                </div>
-                                <div class="col-4">
-                                    <li><a href="#"><b>Дешеве обласне міжміське таксі<br></b>
-                                            <small style="text-align: left">
-                                                Великий парк комфортабельних та надійних автомобілів для міжміських поїздок.
-                                                Дізнайтесь точну вартість на сайті.
+                                                Професійний водій нашої служби акуратно та швидко пережене за вказаною адресою
+                                                або на найближче паркування Ваш автомобіль. Замовити на сайті
                                             </small>
                                         </a>
                                     </li>
@@ -120,6 +92,38 @@
                                             </small>
                                         </a>
                                     </li>
+                               </div>
+
+                            </div>
+                        </div>
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-4">
+                                    <li><a href="#"><b>Таксі в аеропорт Бориспіль та Жуляни<br></b>
+                                            <small style="text-align: left">
+                                                Оцініть  найвигідніші ціни та якісний сервіс на поїздку в аеропорти міста Києва.
+                                                Розрахуйте вартість онлайн.
+                                            </small>
+                                        </a>
+                                    </li>
+                                </div>
+                                <div class="col-4">
+                                    <li><a href="#"><b>Дешеве обласне міжміське таксі<br></b>
+                                            <small style="text-align: left">
+                                                Великий парк комфортабельних автомобілів для міжміських поїздок.
+                                                Дізнайтесь на сайті.
+                                            </small>
+                                        </a>
+                                    </li>
+                                </div>
+                                <div class="col-4">
+                                    <li><a href="#"><b>Зустріч з табличкою<br></b>
+                                            <small style="text-align: left">
+                                                Водій зустріне вас з табличкою біля виходу з паспортного контролю в аеропорту або біля виходу з вагона на вокзалі.
+                                            </small>
+                                        </a>
+                                    </li>
+
                                 </div>
 
 
@@ -129,17 +133,22 @@
                     </ul>
                     </div>
                 </div>
+                <br>
+                <div class="text-center">
+                    <a href="{{route('home')}}" class="gradient-button" target="_blank">Замовити таксі</a>
+                </div>
             </div>
         </div>
-    </div>
 
+    </div>
+    <br>
     <div class="container-fluid">
         <div class="row">
             <div class="col-3">
-                <br>
+
                  <ul class="list-group mb-3">
                     <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="https://m.easy-order-taxi.site/" target="_blank"
+                        <a href="{{route('homeStreet', [$phone = '000', $user_name = 'Новий замовник'])}}" target="_blank"
                            style="text-decoration: none;">Дешевое такси онлайн расчет</a>
                     </li>
                     <li class="list-group-item d-flex justify-content-between lh-sm">
@@ -173,26 +182,10 @@
                 </ul>
             </div>
             <div class="col-9">
-                <div class="container">
-                    <a class="but_vart" href="https://easy-order-taxi.site" target="_blank" title="Версія сайту для мобільних"
-                    style="margin-top: 10px">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
-                            <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
-                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                        </svg>
-                    </a>
-                </div>
-                <div class="container">
-                    <a class="but_vart_vart" href="{{route('homeStreet', [$phone = '000', $user_name = 'Новий замовник'])}}" target="_blank" title="Розрахунок вартості"
-                    style="margin-top: 50px">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
-                            <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
-                        </svg>
-                    </a>
-                </div>
-                <news-component></news-component>
-                 <div class="container" style="text-align: center">
-                     <a href="https://m.easy-order-taxi.site/" target="_blank" style="text-decoration: none;">
+                 <news-component></news-component>
+            </div>
+            <div class="container" style="text-align: center">
+                     <a  href="{{route('homeStreet', [$phone = '000', $user_name = 'Новий замовник'])}}" target="_blank" style="text-decoration: none;">
                         <blockquote class="blockquote-3">
                             <p> <?php echo $quitesArr[$rand] ?></p>
                             <cite>Цитата дня</cite>
@@ -200,10 +193,26 @@
                      </a>
                  </div>
 
-         </div>
+
         </div>
     </div>
 
-
+    <div class="container">
+        <a class="but_vart" href="https://easy-order-taxi.site" target="_blank" title="Версія сайту для мобільних"
+           style="margin-top: 10px">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+                <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+                <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+            </svg>
+        </a>
+    </div>
+    <div class="container">
+        <a class="but_vart_vart" href="{{route('homeStreet', [$phone = '000', $user_name = 'Новий замовник'])}}" target="_blank" title="Розрахунок вартості"
+           style="margin-top: 50px">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+            </svg>
+        </a>
+    </div>
 
 @endsection
