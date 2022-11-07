f41432006244a2e2a951a626e1fcf8014b2fa297
Andrii <sferaved@gmail.com>
2022-11-07 09:59:20 +0200

    Social links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,6 +5,17 @@
         <li class="list-inline-item"><a href="{{route('homeStreet', [$phone = '000', $user_name = 'Новий замовник'])}}" target="_blank">Розрахунок вартості</a></li>
         <li class="list-inline-item"><a href="{{ route('taxi-umovy') }}" target="_blank">Умови</a></li>
         <li class="list-inline-item"><a href="{{ route('feedback') }}" target="_blank">Підтримка</a></li>
-        <li class="list-inline-item"><a href="https://www.facebook.com/people/Taxi-Easy-Ua/100085343706349/" target="_blank">Ми на Фейсбук</a></li>
     </ul>
+    <a href="https://www.facebook.com/people/Taxi-Easy-Ua/100085343706349/"
+       target="_blank" title="Сторінка сайту у Facebook" style="text-decoration: none;">
+        <img src="{{ asset('img/icons8-facebook-circled-24.png') }}">
+    </a>
+    <a href="https://www.linkedin.com/company/taxi-easy-ua/"
+       target="_blank" title="Сторінка сайту у Linkedin" style="text-decoration: none;">
+        <img src="{{ asset('img/icons8-linkedin-24.png') }}">
+    </a>
+    <a href="https://t.me/taxieasyua"
+       target="_blank" title="Сторінка сайту у Telegram" style="text-decoration: none;">
+        <img src="{{ asset('img/icons8-telegram-app-24.png') }}">
+    </a>
 </footer>
