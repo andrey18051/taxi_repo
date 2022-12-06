ae722b3452f0ff461844ce69f2bd446964ed54a8
Andrii <sferaved@gmail.com>
2022-12-06 13:38:21 +0200

    Edit sitemap

--- resources/views/taxi/gdpr.blade.php
+++ resources/views/taxi/gdpr.blade.php
@@ -2,25 +2,159 @@
 
 @section('content')
 
-    <div class="container" style="background-color: hsl(0, 0%, 96%)">
-        <div class="container" style="text-align: center">
-            <h1 class="gradient"><b>Політика конфіденційності</b></h1>
-        </div>
-        <div class="container">
-            <div class="row gx-lg-5 align-items-center">
-                <div class="text-center">
-                    <p class="lead"> Застосовується Загальний регламент захисту персональних даних
-                        <a href="https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:32016R0679">(GDPR)</a>
-                        Європейського союзу
-                    </p>
-                    <p class="lead"> Будь ласка, ознайомтесь з Умовами користування сервісом за
-                        <a href="{{ route('taxi-umovy') }}">посиланням</a>
-                    </p>
+    <div class="container-fluid">
+        <div class="row">
+            @include ('layouts.services')
+
+            <div class="col-lg-9 col-sm-6 col-md-9" >
+
+                <div class="container-fluid">
+                    <a href="{{route('homeCombo')}}"
+                       style="text-decoration: none">
+                        <h4 class="text-center text-primary gradient"> <b>Пропонуємо Вам найсучасніший
+                                сервіс організації поїздок на комфортабельних авто за доступними цінами.</b></h4>
+                    </a>
+
+                </div>
+                <div class="container-fluid">
+                    <div class="row">
+                        <ul class="olderOne">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <li class="col-lg-6 col-sm-12">
+                                        <a href="{{route('orderReklama')}}">
+                                            <h5 class="text-center"><b>Попереднє замовлення</b></h5>
+                                            <small style="text-align: left">
+                                                Можливість оформити замовлення таксі на бажаний час та день.
+                                                Зручно при поїздках до аеропортів та вокзалів Києва. Швидко, надійно та недорого.
+                                                Розрахуйте точну вартість на сайті.
+                                            </small>
+                                        </a>
+                                    </li>
+
+                                    <li class="col-lg-6 col-sm-12">
+                                        <a href="{{route('driverReklama')}}">
+                                            <h5 class="text-center"><b>Послуга "тверезий водій"</b></h5>
+                                            <small style="text-align: left">
+                                                Професійний водій нашої служби акуратно та швидко пережене за вказаною адресою
+                                                або на найближче паркування Ваш автомобіль. Замовити на сайті
+                                            </small>
+                                        </a>
+                                    </li>
+                                </div>
+                            </div>
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <li class="col-lg-6 col-sm-12">
+                                        <a href="{{route('stationReklama')}}">
+                                            <h5 class="text-center"><b>Таксі на вокзал</b></h5>
+                                            <small style="text-align: left">
+                                                Можливість оформити замовлення таксі на бажаний час та день при поїздках до
+                                                вокзалів Києва  та області. Швидко, надійно та недорого. Розрахуйте точну вартість на сайті.
+                                            </small>
+                                        </a>
+                                    </li>
+
+                                    <li class="col-lg-6 col-sm-12">
+                                        <a href="{{route('airportReklama')}}">
+                                            <h5 class="text-center"><b>Таксі в аеропорти Бориспіль та Жуляни</b></h5>
+                                            <small style="text-align: left">
+                                                Оцініть  найвигідніші ціни та якісний сервіс на поїздку в аеропорти міста Києва.
+                                                Розрахуйте вартість онлайн.
+                                            </small>
+                                        </a>
+                                    </li>
+                                </div>
+                            </div>
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <li class="col-lg-6 col-sm-12">
+                                        <a href="{{route('regionReklama')}}">
+                                            <h5 class="text-center"><b>Дешеве обласне міжміське таксі</b></h5>
+                                            <small style="text-align: left">
+                                                Великий парк комфортабельних автомобілів для міжміських поїздок.
+                                                Дізнайтесь на сайті.
+                                            </small>
+                                        </a>
+                                    </li>
+
+                                    <li class="col-lg-6 col-sm-12">
+                                        <a href="{{route('tableReklama')}}">
+                                            <h5 class="text-center"><b>Зустріч водієм таксі з табличкою</b></h5>
+                                            <small style="text-align: left">
+                                                Водій зустріне вас з табличкою біля виходу з паспортного контролю
+                                                або біля виходу з вагона на вокзалі.
+                                            </small>
+                                        </a>
+                                    </li>
+                                </div>
+                            </div>
+                        </ul>
+
+                    </div>
+
+                </div>
+
+                <div class="container" style="background-color: hsl(0, 0%, 96%)">
+                    <br>
+                    <div class="container" style="text-align: center">
+                        <h1 class="gradient"><b>Політика конфіденційності</b></h1>
+                    </div>
+                    <div class="container">
+                        <div class="row gx-lg-5 align-items-center">
+                            <div class="text-center">
+                                <p class="lead" style="text-align: justify"> Застосовується Загальний регламент захисту персональних даних
+                                    <a href="https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:32016R0679">(GDPR)</a>
+                                    Європейського союзу.
+                                </p>
+                                <p class="lead" style="text-align: justify">
+                                    Веб-сайт можна відвідувати та користуватися ним без введення жодної особистої інформації. Деякі частини веб-сайта, які пропонують скачування інформації або послуг, можуть вимагати введення користувачем особистих даних для отримання цих послуг або доступу до особливих розділів або реєстрації як користувача із захистом паролем.
+                                <p class="lead" style="text-align: justify">
+                                    Виконуючи ці дії користувач погоджується с Правиламі конфедійціності на цьому сайті та усвідомлює з якою метою збираються дані, а також те, чи введення інформації є добровільним або обовязковим для користування відповідною послугою.
+                                </p>
+                                <p class="lead"> Будь ласка, ознайомтесь з
+                                    <a href="{{ route('taxi-umovy') }}">Умовами користування сервісом.</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container-fluid" style="margin: 10px">
+                        <a href="{{route('homeCombo')}}"
+                           target="_blank" style="text-decoration: none; color: black">
+                            <h5 style="text-align: center; " class="gradient">
+                                <b>Служба Таксі Лайт Юа – це завжди надійно, комфортно та вигідно. <br>
+                                    Замовьте таксі прям зараз.</b>
+                            </h5>
+                        </a>
+                    </div>
+                    <br>
                 </div>
             </div>
         </div>
-        <br>
+
     </div>
 
+
+    <script>
+        let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+            setTimeout(showSlides, 2000); // Change image every 5 seconds
+        }
+    </script>
 
 @endsection
--- resources/views/taxi/homeNewsCombo.blade.php
+++ resources/views/taxi/homeNewsCombo.blade.php
@@ -44,251 +44,18 @@
     @endif
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-3 col-sm-6 col-md-3">
-                <a href="{{route('homeCombo')}}" target="_blank" style="text-decoration: none;">
-
-                    <p  class="gradient text-opacity-25" id="poslugy">
-                        <b>Послуги нашої служби.</b>
-                    </p>
-                </a>
-                <ul class="list-group mb-3">
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('orderReklama')}}" target="_blank"
-                           style="text-decoration: none;">Попереднє замовлення</a>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('driverReklama')}}" target="_blank"
-                           style="text-decoration: none;">Послуга "тверезий водій"</a>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('stationReklama')}}" target="_blank"
-                           style="text-decoration: none;">Таксі на вокзал</a>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('airportReklama')}}" target="_blank"
-                           style="text-decoration: none;">Таксі в аеропорт Бориспіль та Жуляни</a>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('regionReklama')}}" target="_blank"
-                           style="text-decoration: none;">Дешеве обласне міжміське таксі</a>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('tableReklama')}}" target="_blank"
-                           style="text-decoration: none;">Зустріч з табличкою</a>
-                    </li>
-                </ul>
-                <div class="text-center">
-                    <a href="{{route('homeCombo')}}" class="gradient-button animate-fading" target="_blank">Замовити таксі</a>
-                </div>
-
-                    <p  class="gradient text-opacity-25">
-                        <b>Замовити трансфер</b>
-                    </p>
-                    <div class="row">
-                        <div class="slideshow-container">
-
-                            <div class="mySlides fade">
-                                <a href="{{route('home')}}" target="_blank" style="text-decoration: none;">
-                                    <img src="{{ asset('img/kiyv2.jpg') }}" style="width:100%">
-
-                                </a>
-                            </div>
-
-                            <div class="mySlides fade">
-                                <a href="{{route('stationReklama')}}" target="_blank" style="text-decoration: none;">
-                                    <img src="{{ asset('img/UZ.png') }}" style="width:100%">
-
-                                </a>
-                            </div>
-
-                            <div class="mySlides fade">
-                                <a href="{{route('airportReklama')}}" target="_blank" style="text-decoration: none;">
-                                    <img src="{{ asset('img/borispol.png') }}" style="width:100%">
-
-                                </a>
-                            </div>
-
-                            <div class="mySlides fade">
-                                <a href="{{route('airportReklama')}}" target="_blank" style="text-decoration: none;">
-                                    <img src="{{ asset('img/sikorskogo.png') }}" style="width:100%">
-
-                                </a>
-                            </div>
-
-                            <div class="mySlides fade">
-                                <a href="{{route('stationReklama')}}" target="_blank" style="text-decoration: none;">
-                                    <img src="{{ asset('img/auto.jpeg') }}" style="width:100%">
-
-                                </a>
-                            </div>
-
-                        </div>
-                        <br>
-
-                        <div style="text-align:left">
-                            <span class="dot"></span>
-                            <span class="dot"></span>
-                            <span class="dot"></span>
-                            <span class="dot"></span>
-                            <span class="dot"></span>
-                        </div>
-                    </div>
-                    <ul class="list-group mb-3">
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <a href="{{route('airportReklama')}}" target="_blank"
-                               style="text-decoration: none;">До аеропорту "Бориспіль"</a>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <a href="{{route('airportReklama')}}" target="_blank"
-                               style="text-decoration: none;">До аеропорту "Киів" (Жуляни)</a>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <a href="{{route('stationReklama')}}" target="_blank"
-                               style="text-decoration: none;">До залізничного вокзалу</a>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <a href="{{route('stationReklama')}}" target="_blank"
-                               style="text-decoration: none;">До автовокзалу</a>
-                        </li>
-                    </ul>
-
-                <p  class="gradient text-opacity-25">
-                    <b>Додатково</b>
-                </p>
-
-                <ul class="list-group mb-3">
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('callWorkForm')}}" target="_blank"
-                           style="text-decoration: none;">Робота в таксі</a>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('taxi-gdbr')}}" target="_blank"
-                           style="text-decoration: none;">Конфіденційність</a>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('taxi-umovy')}}" target="_blank"
-                           style="text-decoration: none;">Умови</a>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="{{route('feedback')}}" target="_blank"
-                           style="text-decoration: none;">Підтримка</a>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <a href="https://www.facebook.com/people/Taxi-Easy-Ua/100085343706349/" target="_blank"
-                           style="text-decoration: none;">Ми на Фейсбук</a>
-                    </li>
-                </ul>
-<!--                <p  class="gradient text-opacity-25">
-                    <b>Теги</b>
-                </p>
-                <div class="container-fluid">
-                    <div class="row">
-                            <div class="borderElement"> такси </div>
-                            <div class="borderElement"> uklon </div>
-                            <div class="borderElement"> болт такси </div>
-                            <div class="borderElement"> уклон такси </div>
-                            <div class="borderElement"> такси болт </div>
-                            <div class="borderElement"> убер </div>
-                            <div class="borderElement"> такси макси </div>
-                            <div class="borderElement"> такси уклон </div>
-                            <div class="borderElement"> 838 такси </div>
-                            <div class="borderElement"> такси 838 </div>
-                            <div class="borderElement"> убер такси </div>
-                            <div class="borderElement"> такси белая церковь </div>
-                            <div class="borderElement"> он такси </div>
-                            <div class="borderElement"> уклон телефон </div>
-                            <div class="borderElement"> номер такси </div>
-                            <div class="borderElement"> bolt такси </div>
-                            <div class="borderElement"> uklon номер телефона </div>
-                            <div class="borderElement"> заказать такси </div>
-                            <div class="borderElement"> такси десятка </div>
-                            <div class="borderElement"> такси болт номер телефона </div>
-                            <div class="borderElement"> такси уклон телефон </div>
-                            <div class="borderElement"> uklon драйвер </div>
-                            <div class="borderElement"> вызвать такси </div>
-                            <div class="borderElement"> такси онлайн </div>
-                            <div class="borderElement"> эконом таксиg </div>
-                            <div class="borderElement"> uklon такси </div>
-                            <div class="borderElement"> такси болт номер </div>
-                            <div class="borderElement"> 292 такси </div>
-                            <div class="borderElement"> такси максим номер </div>
-                            <div class="borderElement"> такси 292 </div>
-                            <div class="borderElement"> служба поддержки болт </div>
-                            <div class="borderElement"> uber такси </div>
-                            <div class="borderElement"> такси убер </div>
-                            <div class="borderElement"> дешевое такси </div>
-                            <div class="borderElement"> болт такси телефон </div>
-                            <div class="borderElement"> 579 такси </div>
-                            <div class="borderElement"> уклон для водителей </div>
-                            <div class="borderElement"> любимое такси  </div>
-                            <div class="borderElement"> таксометр </div>
-                            <div class="borderElement"> служба такси </div>
-                            <div class="borderElement"> желтое такси </div>
-                            <div class="borderElement"> мое такси </div>
-                            <div class="borderElement"> оптима такси </div>
-                            <div class="borderElement"> uklon личный кабинет </div>
-                            <div class="borderElement"> uklon онлайн </div>
-                            <div class="borderElement"> такси эконом </div>
-                            <div class="borderElement"> такси межгород </div>
-                            <div class="borderElement"> экспресс такси </div>
-                            <div class="borderElement"> shark taxi </div>
-                            <div class="borderElement"> номер такси болт </div>
-                            <div class="borderElement"> вызов такси </div>
-                            <div class="borderElement"> самое дешевое такси </div>
-                            <div class="borderElement"> такси оптимальное </div>
-                            <div class="borderElement"> такси uber </div>
-                            <div class="borderElement"> такси заказать </div>
-                            <div class="borderElement"> онлайн такси </div>
-                    </div>
-               </div>-->
-                <div class="container-fluid" style="margin-top: 10px">
-                    <p  class="gradient">
-                        <b>Вам також буде цікаво:</b>
-                    </p>
-
-                    <div class="header gradient" >
-                        <a  class="borderElement"
-                            href="{{route('homeCombo')}}" target="_blank">Експрес таксі</a>
-                        <a  class="borderElement"
-                            href="{{route('homeCombo')}}" target="_blank">Дешеве таксі</a>
-                        <a  class="borderElement"
-                            href="{{route('homeCombo')}}" target="_blank">Виклик таксі</a>
-                        <a  class="borderElement"
-                            href="{{route('homeCombo')}}" target="_blank">Таксі міжмісто</a>
-                        <a  class="borderElement"
-                            href="{{route('homeCombo')}}" target="_blank">Онлайн таксі</a>
-                        <a  class="borderElement"
-                            href="{{route('homeCombo')}}" target="_blank">Моє таксі</a>
-                        <a  class="borderElement"
-                            href="{{route('homeMapCombo')}}" target="_blank">Замовлення таксі по мапи</a>
-                        <a  class="borderElement"
-                            @guest
-                            href="{{ route('callBackForm') }}"
-                            @else
-                            href="{{ route('callBackForm-phone', Auth::user()->user_phone) }}"
-                            @endguest>
-                            Допомога у складний час</a>
-                        <a  class="borderElement"
-                            @guest
-                            href="{{ route('callBackForm') }}"
-                            @else
-                            href="{{ route('callBackForm-phone', Auth::user()->user_phone) }}"
-                            @endguest>
-                            Служба підтримки</a>
-                    </div>
-                </div>
-            </div>
+            @include ('layouts.services')
 
              <div class="col-lg-9 col-sm-6 col-md-9" >
 
-                    <div class="container-fluid">
-                        <a href="{{route('homeCombo')}}"
-                                style="text-decoration: none">
-                            <h4 class="text-center text-primary gradient"> <b>Пропонуємо Вам найсучасніший
-                            сервіс організації поїздок на комфортабельних авто за доступними цінами.</b></h4>
-                        </a>
+                 <div class="container-fluid">
+                     <a href="{{route('homeCombo')}}"
+                        style="text-decoration: none">
+                         <h4 class="text-center text-primary gradient"> <b>Пропонуємо Вам найсучасніший
+                                 сервіс організації поїздок на комфортабельних авто за доступними цінами.</b></h4>
+                     </a>
 
-                    </div>
+                 </div>
 
 
                  <div class="container-fluid">
@@ -589,26 +356,26 @@
     </div>
 
 
-        <script>
-            let slideIndex = 0;
-            showSlides();
+    <script>
+        let slideIndex = 0;
+        showSlides();
 
-            function showSlides() {
-                let i;
-                let slides = document.getElementsByClassName("mySlides");
-                let dots = document.getElementsByClassName("dot");
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";
-                }
-                slideIndex++;
-                if (slideIndex > slides.length) {slideIndex = 1}
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-                slides[slideIndex-1].style.display = "block";
-                dots[slideIndex-1].className += " active";
-                setTimeout(showSlides, 2000); // Change image every 5 seconds
+        function showSlides() {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
             }
-        </script>
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+            setTimeout(showSlides, 2000); // Change image every 5 seconds
+        }
+    </script>
 
 @endsection
