1138ba096bbaa16cb00c80fba4133948acb60ac3
Andrii <sferaved@gmail.com>
2022-12-15 11:37:35 +0200

    Fix reports page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,36 +28,76 @@
 
 <div class="container">
     <h3>Отчет по IP</h3>
-    <form action="{{route('reportIpUniqShort')}}" id="form">
-        @csrf
-        <div class="row card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-5">
-                        <input class="form-control" type="date" id="dateFrom" name="dateFrom"
-                               value="{{ date('Y-m-d', strtotime("-1 month")) }}"
-                               autocomplete="off" placeholder="Начало периода">
-                    </div>
-                    <div class="col-md-5">
-                        <input class="form-control" type="date" id="dateTo" name="dateTo"
-                               value="{{  date('Y-m-d') }}"
-                               autocomplete="off" placeholder="Начало периода">
-                    </div>
-                    <div class="col-md-2">
-                        <button class="btn btn-primary" type="submit">
-                            Уникальные IP
-                        </button>
+    <div class="row">
+        <div class="col-md-6">
+            <form action="{{route('reportIpUniqShort')}}" id="form">
+                @csrf
+                <div class="row card">
+                    <div class="card-body">
+                        <div class="row">
+                            <p><b>Уникальные IP</b></p>
+                            <div class="col-md-5">
+                                <input class="form-control" type="date" id="dateFrom" name="dateFrom"
+                                       value="{{ date('Y-m-d', strtotime("-1 month")) }}"
+                                       autocomplete="off" placeholder="Начало периода">
+                            </div>
+                            <div class="col-md-5">
+                                <input class="form-control" type="date" id="dateTo" name="dateTo"
+                                       value="{{  date('Y-m-d') }}"
+                                       autocomplete="off" placeholder="Начало периода">
+                            </div>
+                            <div class="col-md-2">
+                                <button class="btn btn-outline-primary" type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars" viewBox="0 0 16 16">
+                                        <path d="M3 2.5A1.5 1.5 0 0 1 4.5 1h1A1.5 1.5 0 0 1 7 2.5V5h2V2.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5v2.382a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V14.5a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 14.5v-3a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5v3A1.5 1.5 0 0 1 5.5 16h-3A1.5 1.5 0 0 1 1 14.5V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V2.5zM4.5 2a.5.5 0 0 0-.5.5V3h2v-.5a.5.5 0 0 0-.5-.5h-1zM6 4H4v.882a1.5 1.5 0 0 1-.83 1.342l-.894.447A.5.5 0 0 0 2 7.118V13h4v-1.293l-.854-.853A.5.5 0 0 1 5 10.5v-1A1.5 1.5 0 0 1 6.5 8h3A1.5 1.5 0 0 1 11 9.5v1a.5.5 0 0 1-.146.354l-.854.853V13h4V7.118a.5.5 0 0 0-.276-.447l-.895-.447A1.5 1.5 0 0 1 12 4.882V4h-2v1.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V4zm4-1h2v-.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V3zm4 11h-4v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14zm-8 0H2v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
-    </form>
 
-    <form action="{{route('reportIpRoute')}}" id="form">
+        <div class="col-md-6">
+            <form action="{{route('reportIpRoute')}}" id="form">
+                @csrf
+                <div class="row card">
+                    <div class="card-body">
+                        <div class="row">
+                            <p><b>Расчеты поездок</b></p>
+                            <div class="col-md-5">
+                                <input class="form-control" type="date" id="dateFrom" name="dateFrom"
+                                       value="{{ date('Y-m-d', strtotime("-1 month")) }}"
+                                       autocomplete="off" placeholder="Начало периода">
+                            </div>
+                            <div class="col-md-5">
+                                <input class="form-control" type="date" id="dateTo" name="dateTo"
+                                       value="{{  date('Y-m-d') }}"
+                                       autocomplete="off" placeholder="Начало периода">
+                            </div>
+                            <div class="col-md-2">
+                                <button class="btn btn-outline-primary" type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars" viewBox="0 0 16 16">
+                                        <path d="M3 2.5A1.5 1.5 0 0 1 4.5 1h1A1.5 1.5 0 0 1 7 2.5V5h2V2.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5v2.382a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V14.5a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 14.5v-3a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5v3A1.5 1.5 0 0 1 5.5 16h-3A1.5 1.5 0 0 1 1 14.5V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V2.5zM4.5 2a.5.5 0 0 0-.5.5V3h2v-.5a.5.5 0 0 0-.5-.5h-1zM6 4H4v.882a1.5 1.5 0 0 1-.83 1.342l-.894.447A.5.5 0 0 0 2 7.118V13h4v-1.293l-.854-.853A.5.5 0 0 1 5 10.5v-1A1.5 1.5 0 0 1 6.5 8h3A1.5 1.5 0 0 1 11 9.5v1a.5.5 0 0 1-.146.354l-.854.853V13h4V7.118a.5.5 0 0 0-.276-.447l-.895-.447A1.5 1.5 0 0 1 12 4.882V4h-2v1.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V4zm4-1h2v-.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V3zm4 11h-4v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14zm-8 0H2v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-6">
+            <form action="{{route('reportIpPage')}}" id="form">
         @csrf
         <div class="row card">
             <div class="card-body">
                 <div class="row">
+                    <p><b>Страницы сайта</b></p>
                     <div class="col-md-5">
                         <input class="form-control" type="date" id="dateFrom" name="dateFrom"
                                value="{{ date('Y-m-d', strtotime("-1 month")) }}"
@@ -69,89 +109,80 @@
                                autocomplete="off" placeholder="Начало периода">
                     </div>
                     <div class="col-md-2">
-                        <button class="btn btn-primary" type="submit">
-                            Расчеты поездок
+                        <button class="btn btn-outline-primary" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars" viewBox="0 0 16 16">
+                                <path d="M3 2.5A1.5 1.5 0 0 1 4.5 1h1A1.5 1.5 0 0 1 7 2.5V5h2V2.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5v2.382a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V14.5a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 14.5v-3a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5v3A1.5 1.5 0 0 1 5.5 16h-3A1.5 1.5 0 0 1 1 14.5V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V2.5zM4.5 2a.5.5 0 0 0-.5.5V3h2v-.5a.5.5 0 0 0-.5-.5h-1zM6 4H4v.882a1.5 1.5 0 0 1-.83 1.342l-.894.447A.5.5 0 0 0 2 7.118V13h4v-1.293l-.854-.853A.5.5 0 0 1 5 10.5v-1A1.5 1.5 0 0 1 6.5 8h3A1.5 1.5 0 0 1 11 9.5v1a.5.5 0 0 1-.146.354l-.854.853V13h4V7.118a.5.5 0 0 0-.276-.447l-.895-.447A1.5 1.5 0 0 1 12 4.882V4h-2v1.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V4zm4-1h2v-.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V3zm4 11h-4v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14zm-8 0H2v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14z"/>
+                            </svg>
                         </button>
                     </div>
                 </div>
             </div>
         </div>
     </form>
+        </div>
+        <div class="col-md-6">
+            <form action="{{route('reportIpUniq')}}" id="form">
+        @csrf
+        <div class="row card">
+            <div class="card-body">
+                <div class="row">
+                    <p><b>Расшифровка IP</b></p>
+                    <div class="col-md-5">
+                        <input class="form-control" type="date" id="dateFrom" name="dateFrom"
+                               value="{{ date('Y-m-d', strtotime("-1 month")) }}"
+                               autocomplete="off" placeholder="Начало периода">
+                    </div>
+                    <div class="col-md-5">
+                        <input class="form-control" type="date" id="dateTo" name="dateTo"
+                               value="{{  date('Y-m-d') }}"
+                               autocomplete="off" placeholder="Начало периода">
+                    </div>
+                    <div class="col-md-2">
+                        <button class="btn btn-outline-primary" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars" viewBox="0 0 16 16">
+                                <path d="M3 2.5A1.5 1.5 0 0 1 4.5 1h1A1.5 1.5 0 0 1 7 2.5V5h2V2.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5v2.382a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V14.5a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 14.5v-3a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5v3A1.5 1.5 0 0 1 5.5 16h-3A1.5 1.5 0 0 1 1 14.5V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V2.5zM4.5 2a.5.5 0 0 0-.5.5V3h2v-.5a.5.5 0 0 0-.5-.5h-1zM6 4H4v.882a1.5 1.5 0 0 1-.83 1.342l-.894.447A.5.5 0 0 0 2 7.118V13h4v-1.293l-.854-.853A.5.5 0 0 1 5 10.5v-1A1.5 1.5 0 0 1 6.5 8h3A1.5 1.5 0 0 1 11 9.5v1a.5.5 0 0 1-.146.354l-.854.853V13h4V7.118a.5.5 0 0 0-.276-.447l-.895-.447A1.5 1.5 0 0 1 12 4.882V4h-2v1.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V4zm4-1h2v-.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V3zm4 11h-4v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14zm-8 0H2v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-6">
+            <form action="{{route('reportIpOrder')}}" id="form">
+        @csrf
+        <div class="row card">
+            <div class="card-body">
+                <div class="row">
+                    <p><b>Заказы поездок</b></p>
+                    <div class="col-md-5">
+                        <input class="form-control" type="date" id="dateFrom" name="dateFrom"
+                               value="{{ date('Y-m-d', strtotime("-1 month")) }}"
+                               autocomplete="off" placeholder="Начало периода">
+                    </div>
+                    <div class="col-md-5">
+                        <input class="form-control" type="date" id="dateTo" name="dateTo"
+                               value="{{  date('Y-m-d') }}"
+                               autocomplete="off" placeholder="Начало периода">
+                    </div>
+                    <div class="col-md-2">
+                        <button class="btn btn-outline-primary" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars" viewBox="0 0 16 16">
+                                <path d="M3 2.5A1.5 1.5 0 0 1 4.5 1h1A1.5 1.5 0 0 1 7 2.5V5h2V2.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5v2.382a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V14.5a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 14.5v-3a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5v3A1.5 1.5 0 0 1 5.5 16h-3A1.5 1.5 0 0 1 1 14.5V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V2.5zM4.5 2a.5.5 0 0 0-.5.5V3h2v-.5a.5.5 0 0 0-.5-.5h-1zM6 4H4v.882a1.5 1.5 0 0 1-.83 1.342l-.894.447A.5.5 0 0 0 2 7.118V13h4v-1.293l-.854-.853A.5.5 0 0 1 5 10.5v-1A1.5 1.5 0 0 1 6.5 8h3A1.5 1.5 0 0 1 11 9.5v1a.5.5 0 0 1-.146.354l-.854.853V13h4V7.118a.5.5 0 0 0-.276-.447l-.895-.447A1.5 1.5 0 0 1 12 4.882V4h-2v1.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V4zm4-1h2v-.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V3zm4 11h-4v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14zm-8 0H2v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+        </div>
+    </div>
 
-    <form action="{{route('reportIpPage')}}" id="form">
-        @csrf
-        <div class="row card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-5">
-                        <input class="form-control" type="date" id="dateFrom" name="dateFrom"
-                               value="{{ date('Y-m-d', strtotime("-1 month")) }}"
-                               autocomplete="off" placeholder="Начало периода">
-                    </div>
-                    <div class="col-md-5">
-                        <input class="form-control" type="date" id="dateTo" name="dateTo"
-                               value="{{  date('Y-m-d') }}"
-                               autocomplete="off" placeholder="Начало периода">
-                    </div>
-                    <div class="col-md-2">
-                        <button class="btn btn-primary" type="submit">
-                            Страницы сайта
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
-
-    <form action="{{route('reportIpUniq')}}" id="form">
-        @csrf
-        <div class="row card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-5">
-                        <input class="form-control" type="date" id="dateFrom" name="dateFrom"
-                               value="{{ date('Y-m-d', strtotime("-1 month")) }}"
-                               autocomplete="off" placeholder="Начало периода">
-                    </div>
-                    <div class="col-md-5">
-                        <input class="form-control" type="date" id="dateTo" name="dateTo"
-                               value="{{  date('Y-m-d') }}"
-                               autocomplete="off" placeholder="Начало периода">
-                    </div>
-                    <div class="col-md-2">
-                        <button class="btn btn-primary" type="submit">
-                            Расшифровка IP
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
-
-    <form action="{{route('reportIpOrder')}}" id="form">
-        @csrf
-        <div class="row card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-5">
-                        <input class="form-control" type="date" id="dateFrom" name="dateFrom"
-                               value="{{ date('Y-m-d', strtotime("-1 month")) }}"
-                               autocomplete="off" placeholder="Начало периода">
-                    </div>
-                    <div class="col-md-5">
-                        <input class="form-control" type="date" id="dateTo" name="dateTo"
-                               value="{{  date('Y-m-d') }}"
-                               autocomplete="off" placeholder="Начало периода">
-                    </div>
-                    <div class="col-md-2">
-                        <button class="btn btn-primary" type="submit">
-                            Заказы поездок
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
 </div>
 
 <script defer src="https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCoyJk5j4GRS41GYwZTRJduPnV5k8SDCoc"></script>
