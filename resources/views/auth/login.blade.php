a84ba6a6ca7045c4c8a942f3485ad4b2d9afed95
Andrii <sferaved@gmail.com>
2022-12-20 18:38:15 +0200

    Fix Register Telegram form email

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -106,7 +106,7 @@
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Укажите Ваш email</h5>
+                                                <h5 class="modal-title" id="exampleModalLabel">Вкажіть Ваш email</h5>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
                                             </div>
                                             <div class="modal-body">
@@ -123,17 +123,15 @@
                                                                 <strong>{{ $message }}</strong>
                                                             </span>
                                                     @enderror
-
-                                                    <button type="submit" class="btn btn-primary" >
-                                                        Отправить
-                                                    </button>
+                                                    <div class="modal-footer">
+                                                        <button type="submit" class="btn btn-primary" >
+                                                            Відправити
+                                                        </button>
+                                                    </div>
                                                 </form>
 
                                             </div>
-                                            <!--                                                    <div class="modal-footer">
-                                                                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
-                                                                                                    <button type="button" class="btn btn-primary">Сохранить изменения</button>
-                                                                                                </div>-->
+
                                         </div>
                                     </div>
                                 </div>
