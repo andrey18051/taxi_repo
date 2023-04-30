09eb306a2108c96abc22ac31c918ad1df41e31d6
Andrii <sferaved@gmail.com>
2023-04-30 13:47:40 +0300

    Add title to copy button

--- resources/views/widgets/main.blade.php
+++ resources/views/widgets/main.blade.php
@@ -44,7 +44,7 @@
     @endif
     <div class="container-fluid">
         <div class="row">
-            @include ('layouts.services')
+            @include ('layouts.servicesShort')
 
             <div class="col-lg-9 col-sm-6 col-md-9" >
 
@@ -68,7 +68,7 @@
                             <div class="accordion-body">
                                 <div class="card">
                                     <div class="card-body">
-                                        <div class="container">
+                                        <div class="container-fluid">
                                             <div class="row">
                                                 <iframe
                                                     src="https://m.easy-order-taxi.site/widgets/job" frameborder="0" marginheight=0 marginwidth=0 height="1000px"  width="600px">
@@ -82,7 +82,7 @@
                                     <div class="card-body">
                                         <div class="container">
                                             <div class="row">
-                                                <a onclick="myFunction()" class="col-12 col-form-label text-md-end">
+                                                <a onclick="myFunction()" class="col-12 col-form-label text-md-end" title="Копіювати">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
                                                         <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
                                                         <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
