ea3172b2b9738003ee373cf0f6343ca22861899b
Andrii <sferaved@gmail.com>
2023-07-26 15:31:47 +0300

    2.715

--- app/Http/Controllers/AndroidTestController.php
+++ app/Http/Controllers/AndroidTestController.php
@@ -30,7 +30,7 @@ class AndroidTestController extends Controller
     public function version()
     {
         $response_error["resp_result"] = 200;
-        $response_error["message"] = "2.714";
+        $response_error["message"] = "2.715";
 
         return  response($response_error, 200)
             ->header('Content-Type', 'json');
